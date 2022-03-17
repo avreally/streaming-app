ea1a94c6ef14dc9c1d57613b7d3ae2bfb2d7892a
Valeria <waleria.shadrina@gmail.com>
2022-03-17 13:48:08 +0100

    Started implementing user authorization with Github

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,46 @@
 import express from "express";
 import { connectToDatabase } from "./services/database.service";
 import { itemsRouter } from "./routes/items.router";
+import "dotenv/config";
+import axios from "axios";
 
 const app = express();
 app.use(express.json());
+
+let access_token = "";
+
+const clientId = process.env.CLIENT_ID;
+const clientSecret = process.env.CLIENT_SECRET;
+
+app.get("/github/callback", (_req, res) => {
+  const requestToken = _req.query.code;
+
+  axios({
+    method: "post",
+    url: `https://github.com/login/oauth/authorize?client_id=${clientId}&client_secret=${clientSecret}&code=${requestToken}`,
+    headers: {
+      accept: "application/json",
+    },
+  }).then((response) => {
+    access_token = response.data.access_token;
+    res.redirect("/success");
+  });
+  // res.redirect(
+  //   `https://github.com/login/oauth/authorize?client_id=${clientId}`
+  // );
+});
+
+app.get("/success", function (_req, res) {
+  axios({
+    method: "get",
+    url: `https://api.github.com/user`,
+    headers: {
+      Authorization: "token " + access_token,
+    },
+  }).then((response) => {
+    res.render("pages/success", { userData: response.data });
+  });
+});
 
 const PORT = 3000;
 
---
+++ server/routes/github.router.ts
@@ -0,0 +1,6 @@
+// import express from "express";
+//
+// export const githubRouter = express.Router();
+
+// const clientId = process.env.CLIENT_ID;
+// const clientSecret = process.env.CLIENT_SECRET;
