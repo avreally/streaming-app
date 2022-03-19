07755aeddf219074c16301e90eea1a0c38bc08c8
Valeria <waleria.shadrina@gmail.com>
2022-03-19 19:14:50 +0100

    Refactored Github authorization part, implemented getting user data from Github API

--- server/index.ts
+++ server/index.ts
@@ -7,39 +7,44 @@ import axios from "axios";
 const app = express();
 app.use(express.json());
 
-let access_token = "";
-
 const clientId = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
 
-app.get("/github/callback", (_req, res) => {
-  const requestToken = _req.query.code;
-
-  axios({
-    method: "post",
-    url: `https://github.com/login/oauth/authorize?client_id=${clientId}&client_secret=${clientSecret}&code=${requestToken}`,
-    headers: {
-      accept: "application/json",
-    },
-  }).then((response) => {
-    access_token = response.data.access_token;
-    res.redirect("/success");
-  });
-  // res.redirect(
-  //   `https://github.com/login/oauth/authorize?client_id=${clientId}`
-  // );
+app.get("/login", (_req, res) => {
+  res.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${clientId}`
+  );
 });
 
-app.get("/success", function (_req, res) {
-  axios({
-    method: "get",
-    url: `https://api.github.com/user`,
-    headers: {
-      Authorization: "token " + access_token,
-    },
-  }).then((response) => {
-    res.render("pages/success", { userData: response.data });
-  });
+let token: string | undefined = "";
+
+app.get("/oauth-callback", (req, res) => {
+  const body = {
+    client_id: clientId,
+    client_secret: clientSecret,
+    code: req.query.code,
+  };
+  const options = { headers: { accept: "application/json" } };
+
+  axios
+    .post(`https://github.com/login/oauth/access_token`, body, options)
+    .then((res) => res.data["access_token"])
+    .then(async (_token) => {
+      token = _token;
+      console.log("My token:", token);
+      res.json({ ok: 1 });
+
+      const { data } = await axios({
+        url: "https://api.github.com/user",
+        method: "get",
+        headers: {
+          Authorization: `token ${token}`,
+        },
+      });
+      console.log(data.id, data.name); // { id, email, name, login, avatar_url }
+      return data;
+    })
+    .catch((err) => res.status(500).json({ message: err.message }));
 });
 
 const PORT = 3001;
