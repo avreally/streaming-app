88468cac2dd2d5b946d1da203d90d631fa8dc602
Valeria <waleria.shadrina@gmail.com>
2025-04-23 13:56:40 +0200

    add Playlist type and update User type to include playlists

---
+++ server/types/playlist.ts
@@ -0,0 +1,4 @@
+export type Playlist = {
+  playlistId: number;
+  title: string;
+};
--- server/types/user.ts
+++ server/types/user.ts
@@ -1,6 +1,9 @@
+import { Playlist } from "./playlist.js";
+
 export type User = {
   githubUserId: number;
   userName: string;
   favouriteTrackIds: number[];
   avatarUrl: string;
+  playlists: Playlist[];
 };
