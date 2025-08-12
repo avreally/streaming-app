fde96f0083fee394c6fcf3de9e5109462b0294a4
Valeria <waleria.shadrina@gmail.com>
2025-08-12 21:24:42 +0200

    Refactor Track component to improve playlist selection functionality and layout structure

--- src/components/Track/Track.tsx
+++ src/components/Track/Track.tsx
@@ -1,3 +1,8 @@
+import { useState } from "react";
+import { Button } from "../Button/Button";
+import Modal from "../Modal/Modal";
+import PlaylistSelector from "../PlaylistSelector/PlaylistSelector";
+import { IconCheck } from "../Icons/IconCheck";
 import styles from "./Track.module.css";
 
 type TrackProps = {
@@ -7,14 +12,47 @@ type TrackProps = {
   artist: string;
 };
 
-export const Track = ({ title, url, artist }: TrackProps) => {
+export const Track = ({ id, title, url, artist }: TrackProps) => {
+  const [showModal, setShowModal] = useState(false);
+  const [isPlaylistSelected, setIsPlaylistSelected] = useState(false);
+  const [playlistTitle, setPlaylistTitle] = useState("");
+
+  function handleSelectPlaylist(title: string) {
+    setShowModal(false);
+    setIsPlaylistSelected(true);
+    setPlaylistTitle(title);
+  }
+
   return (
     <li className={styles.track}>
-      <audio className={styles.audio} controls src={url}></audio>
-      <div className={styles.info}>
-        <h3 className={styles.title}>{title}</h3>
-        <p className={styles.artist}>{artist}</p>
+      <div className={styles.wrapper}>
+        <audio className={styles.audio} controls src={url}></audio>
+        <div className={styles.info}>
+          <h3 className={styles.title}>{title}</h3>
+          <p className={styles.artist}>{artist}</p>
+        </div>
       </div>
+      <Button onClick={() => setShowModal(true)}>Add to playlist...</Button>
+      {showModal && (
+        <Modal
+          isShown={showModal}
+          onCancel={() => setShowModal(false)}
+          variant="playlist-selector"
+        >
+          <PlaylistSelector trackId={id} onSelect={handleSelectPlaylist} />
+        </Modal>
+      )}
+      {isPlaylistSelected && (
+        <Modal
+          isShown={isPlaylistSelected}
+          onCancel={() => setIsPlaylistSelected(false)}
+        >
+          <div className={styles.container}>
+            <IconCheck className={styles.checkmark} />
+            Track added to the {playlistTitle} playlist!
+          </div>
+        </Modal>
+      )}
     </li>
   );
 };
